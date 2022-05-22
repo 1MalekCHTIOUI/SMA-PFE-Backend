b2c961ce14e1c9da3d2e1510b651347ed306072e
Malek Chtioui <chtiouimalek001@gmail.com>
2022-05-22 18:12:11 +0100

    commit

--- Controllers/upload.js
+++ Controllers/upload.js
@@ -1,8 +1,5 @@
 exports.upload = async (req, res, next) => {
-  const url = req.protocol + "://" + req.get("host");
-  res
-    .status(200)
-    .json({ upload: url + "/public/uploads/" + req.file.filename });
+  res.status(200).json({ upload: req.file.filename });
 };
 
 exports.getFile = async (req, res, next) => {};
