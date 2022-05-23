9ac877655dbc76353ba3381be3af88da6f7ac668
Malek Chtioui <chtiouimalek001@gmail.com>
2022-05-23 14:57:35 +0100

    Additional route

--- Routes/post.js
+++ Routes/post.js
@@ -2,13 +2,14 @@ const router = require("express").Router();
 const postController = require("../Controllers/posts");
 const commentController = require("../Controllers/comments");
 
-router.post("/", postController.newPost);
 router.post("/comment", commentController.newComment);
 router.get("/comment/:postId", commentController.getPostComments);
-router.get("/", postController.getPosts);
 router.delete("/comment/:commentId", commentController.deleteComment);
-router.put("/like/:commentId", commentController.likeComment);
-router.put("/unlike/:postId", commentController.unlikeComment);
+router.put("/comment/like/:commentId", commentController.likeComment);
+router.put("/comment/unlike/:commentId", commentController.unlikeComment);
+
+router.post("/", postController.newPost);
+router.get("/", postController.getPosts);
 router.get("/:userId", postController.getUserPosts);
 router.put("/like/:postId", postController.likePost);
 router.put("/unlike/:postId", postController.unlikePost);
