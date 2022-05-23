8152120cc815d9336d1d1dfd2799ce0b62b3222e
Malek Chtioui <chtiouimalek001@gmail.com>
2022-05-23 14:53:33 +0100

    Additional route

--- Routes/post.js
+++ Routes/post.js
@@ -7,6 +7,8 @@ router.post("/comment", commentController.newComment);
 router.get("/comment/:postId", commentController.getPostComments);
 router.get("/", postController.getPosts);
 router.delete("/comment/:commentId", commentController.deleteComment);
+router.put("/like/:commentId", commentController.likeComment);
+router.put("/unlike/:postId", commentController.unlikeComment);
 router.get("/:userId", postController.getUserPosts);
 router.put("/like/:postId", postController.likePost);
 router.put("/unlike/:postId", postController.unlikePost);
