e6b2e6e35f6450d087ad8038b40923679bc277b2
Malek Chtioui <chtiouimalek001@gmail.com>
2022-05-26 19:05:48 +0100

    API Changes

--- Controllers/upload.js
+++ Controllers/upload.js
@@ -1,5 +1,6 @@
 const { cloudinary } = require("../Utils/cloudinary");
-
+const IMAGE_URL_LENGTH = 43;
+const START_TO_IMAGE = 84;
 exports.upload = async (req, res, next) => {
   // res.status(200).json({ upload: req.file.filename });
   // console.log(req.file);
@@ -8,8 +9,14 @@ exports.upload = async (req, res, next) => {
     const response = await cloudinary.uploader.upload(fileStr, {
       upload_preset: "sma_uploads",
     });
-    console.log(response);
-    res.status(200).json({ upload: response.original_filename });
+
+    const url = response.secure_url;
+    const filename = url.substring(
+      START_TO_IMAGE,
+      START_TO_IMAGE + IMAGE_URL_LENGTH,
+    );
+
+    res.status(200).json({ upload: filename });
   } catch (error) {
     console.log(error);
     console.log(req.body.data);
